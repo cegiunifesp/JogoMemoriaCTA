bc122412848323464a354b1be26abb33d7aca7a4
renanruan <37370013+renanruan@users.noreply.github.com>
2018-04-13 14:54:42 -0300

    Renan

---
+++ JogoMemoriaBasico/Assets/Scripts/CartaBehavior.cs
@@ -0,0 +1,162 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CartaBehavior : MonoBehaviour {
+
+    // Identificao da carta
+    private int id_carta;
+
+    // Sprites usados
+    private Sprite sprite_frente;
+    private Sprite sprite_costas;
+
+    // Estados
+    private bool selecionado;
+    private bool pareado;
+
+    // Renderer
+    private SpriteRenderer sr;
+
+    //Animacao
+    private int girar;
+    private float timer;
+    private const float timer_virar = 0.5f;
+
+
+    void Awake()
+    {
+        // Inicia estados e Renderer
+        sr = GetComponent<SpriteRenderer>();
+        selecionado = false;
+        pareado = false;
+        girar = 0;
+        timer = timer_virar;
+    }
+
+    // Ajusta os sprites e a id
+    public void setCarta(Sprite sf, Sprite sc, int id)
+    {
+        sprite_frente = sf;
+        sprite_costas = sc;
+        id_carta = id;
+        sr.sprite = sc;
+    }
+
+    // Retorna se esta selecionado
+    // Usado no testeSelecionados para pegar quem esta selecionado
+    public bool getSelecao()
+    {
+        return selecionado;
+    }
+
+    // Retorna ID
+    // Usado no testeSelecionados para ver se os selecionados tem a mesma ID
+    public int getID()
+    {
+        return id_carta;
+    }
+
+
+    // Quando encontrado o par ajusta o estado
+    public void setPareado()
+    {
+        pareado = true;
+        selecionado  = false;
+    }
+
+    // Quando nao encontrado o par ajusta o estado e o sprite
+    public void setNonPareado()
+    {
+        selecionado = false;
+        mudaSprite();
+    }
+
+    // Quando clickado
+    void clickado()
+    {
+        // Se ja tem dois selecionados ou ja esta pareado nao faz nada
+        if((CartaController.controller.getSelecionados() == 2) || pareado)
+            return;
+
+        // Se nao esta selecionado atualiza estado, muda sprite e aumenta o valor dos selecionados
+        if(!selecionado)
+        {
+            selecionado = true;
+            mudaSprite();
+            CartaController.controller.addSelecionados(1);
+        }
+        else // Se ja estava selecionado desvira (?) talvez deva tirar
+        {
+            selecionado = false;
+            mudaSprite();
+            CartaController.controller.addSelecionados(-1);
+        }
+    }
+
+    // faz a animacao de mudanca de sprite
+    private void mudaSprite()
+    {
+        // Muda Girar
+        girar = 1;
+    }
+
+    void OnMouseDown()
+    {
+        clickado();
+    }
+
+    void Update()
+    {
+        // Se é pra girar
+        if (girar != 0)
+        {
+
+            // gira
+            transform.Rotate(new Vector3(0, 180 * girar, 0) * Time.deltaTime);
+
+            // Se ja foi ate a metade do caminho
+            if ((transform.eulerAngles.y > 90) && (transform.eulerAngles.y < 270))
+            {
+                // muda direcao do giro
+                girar = -1;
+                transform.eulerAngles = new Vector3(0, 90, 0);
+
+                // muda sprite
+                if (selecionado)
+                    sr.sprite = sprite_frente;
+                else
+                    sr.sprite = sprite_costas;
+            }
+
+            // se terminou caminho
+            if (transform.eulerAngles.y > 270)
+            {
+                // para
+                girar = 0;
+                transform.eulerAngles = new Vector3(0, 0, 0);
+
+                // caso tenha 2 manda testar
+                if (CartaController.controller.getSelecionados() == 2)
+                    timer = 0f;
+                else
+                    timer = timer_virar;
+
+            }
+                
+        }
+        else
+        {
+            // quando der o timer_virar testa
+            if (timer < timer_virar)
+                timer += Time.deltaTime;
+            else if (timer > timer_virar)
+            {
+                timer = timer_virar;
+                CartaController.controller.testeSelecionados(); // Se o valor dos selecionados agora é 2 faz o teste
+            }
+        }
+
+    }
+
+}
---
+++ JogoMemoriaBasico/Assets/Scripts/CartaController.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CartaController : MonoBehaviour{
+
+    // Controller Atual
+    static public CartaController controller = null;
+
+    // Lista de sprites usados nas cartas
+    public List<Sprite> Cartas;
+    public Sprite CartaCostas;
+
+    // Sample do objeto
+    public GameObject Carta;
+
+    // Vetor de posicoes das cartas
+    private List<Vector3> matriz;
+
+    // Tamanho do lugar onde serao distribuidas as cartas
+    public float largura;
+    public float altura;
+
+    // Quantidade de cartas
+    public int num_cartas_h, num_cartas_v;
+
+    // Quantidade de cartas selecionadas
+    private int selecionados;
+    
+    void Start()
+    {
+        
+        selecionados = 0; // Inicia com 0 selecionadas
+
+        // Faz com que exista apenas um controller
+        if(controller == null)
+            controller = this;
+        else
+            return;
+
+
+        // Cria a matriz de posicoes
+        if (createMatriz() == 0)
+        {
+            Debug.Log("Erro na quantidade das cartas");
+            return;
+        }
+
+        // Pra cada sprite cria dois objetos cartas
+        foreach (Sprite s in Cartas)
+        {
+            GameObject c1 = Instantiate(Carta,transform);
+            GameObject c2 = Instantiate(Carta,transform);
+
+            int id = Cartas.IndexOf(s);
+
+            // Faz elas serem iguais
+            c1.GetComponent<CartaBehavior>().setCarta(s, CartaCostas, id);
+            c2.GetComponent<CartaBehavior>().setCarta(s, CartaCostas, id);
+
+            // Coloca elas em posicoes aleatorias
+            id = Random.Range(0, matriz.Count);
+            c1.transform.position = matriz[id];
+            matriz.RemoveAt(id);
+
+            id = Random.Range(0, matriz.Count);
+            c2.transform.position = matriz[id];
+            matriz.RemoveAt(id);
+
+        }
+    }
+
+    // cria a matriz
+    private int createMatriz()
+    {
+        // Se o numero de cartas nao bate com o de sprites da erro
+        if (num_cartas_h * num_cartas_v != Cartas.Count * 2)
+            return 0;
+
+        matriz = new List<Vector3>();
+
+        // Espaçamentos entre as cartas
+        float delta_x = largura / (num_cartas_h - 1);
+        float delta_y = altura / (num_cartas_v - 1);
+
+        // Cria pontos
+        for (int i = 0; i < num_cartas_h; i++)
+            for (int j = 0; j < num_cartas_v; j++)
+                matriz.Add(new Vector3(i * delta_x - largura/2 , j * delta_y - altura/2, -1));
+
+        return 1;
+
+    }
+
+    // Sempre que a selecao de uma carta muda é chamada essa funcao pra atualizar o numero de selecionados
+    public void addSelecionados(int i)
+    {
+        selecionados += i;
+    }
+
+    // retorna o numero de selecionados
+    public int getSelecionados()
+    {
+        return selecionados;
+    }
+
+    // teste se todos os selecionados sao iguais
+    public void testeSelecionados()
+    {
+        // Faz teste
+
+        // pega o primeiro selecionado
+        CartaBehavior selecionado = null;
+
+        // pra cada um faz o teste
+        foreach (CartaBehavior c in transform.GetComponentsInChildren<CartaBehavior>())
+            if (c.getSelecao())
+                if (selecionado == null)
+                    selecionado = c;
+                else if (selecionado.getID() == c.getID())
+                {
+                    print("Faz par");
+                    selecionado.setPareado();
+                    c.setPareado();
+                    addSelecionados(-2);
+                }
+                else
+                {
+                print("nao Faz par");
+                    selecionado.setNonPareado();
+                    c.setNonPareado();
+                    addSelecionados(-2);
+
+                }
+    }
+
+}
+
